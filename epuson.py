27d7a5bf2a5305377aea51aa3a5e5a2446d5d118
matsu <50922910+mtsml@users.noreply.github.com>
2021-06-13 20:14:41 +0900

    ファイル名を変更

--- epuson.py
+++ epuson.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import pandas_profiling as pdq
+
+import optuna.integration.lightgbm as lgb
+from sklearn.model_selection import train_test_split
 
 from db.db import Db
+
 
 db = Db()
 conn = db.get_connection()
@@ -15,7 +20,6 @@ select
 	(case when jockey_id = 5339 then 1 else 0 end) rumale_flg,
 	(case when jockey_id = 711 then 1 else 0 end) goto_flg,
 	(case when jockey_id = 1009 then 1 else 0 end) shibata_flg,	
-	jockey_id::text,
 	sex,
 	age,
 	odds,
@@ -31,4 +35,11 @@ where race_date > '2001-01-01'
 '''
 
 df = pd.read_sql(con=conn, sql=sql)
-df.columns
+df_new = pd.get_dummies(df)
+
+y = df['chakujun']
+x = df.drop(['chakujun'], axis=1)
+
+
+X_train, X_test, y_train, y_test = train_test_split(x, y)
+X_train, x_val, y_train, y_val = train_test_split(X_train, y_train)
